1d8ec501693bc47a634c8e6d1bd3b55f4f65fbd7
Miko <lolicon28111@gmail.com>
2025-10-28 16:39:29 +0700

    Add module_2

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,13 +1,25 @@
 package com.example.demo.controllers;
 
+import com.example.demo.service.UtilityService;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final UtilityService utilityService;
+    private final ModelMapper modelMapper;
+
+    public HelloController(UtilityService utilityService, ModelMapper modelMapper) {
+        this.utilityService = utilityService;
+        this.modelMapper = modelMapper;
+    }
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        String empCode = utilityService.generateEmployeeCode();
+        String name = utilityService.formatName("john");
+        return "Hello " + name + "! Your code is: " + empCode;
     }
 }
