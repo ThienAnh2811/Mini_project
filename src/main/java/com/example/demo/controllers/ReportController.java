cd2057c33fa3c1d5b3210ed5f509fe1ea96f9acf
Miko <lolicon28111@gmail.com>
2025-10-31 15:41:22 +0700

    Add module 10

--- src/main/java/com/example/demo/controllers/ReportController.java
+++ src/main/java/com/example/demo/controllers/ReportController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/report")
 @RequiredArgsConstructor
@@ -17,5 +19,14 @@ public class ReportController {
     public ResponseEntity<Long> getEmployeeCount() {
         long count = reportService.getEmployeeCount();
         return ResponseEntity.ok(count);
+    }
+    @GetMapping("/department-count")
+    public Map<String, Long> countByDepartment() {
+        return reportService.getEmployeeCountByDepartment();
+    }
+
+    @GetMapping("/total-employees")
+    public long totalEmployees() {
+        return reportService.getTotalEmployees();
     }
 }
--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -2,9 +2,17 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findByNameContainingIgnoreCase(String name);
     List<Employee> findByDepartment_NameContainingIgnoreCase(String name);
+    @Query("SELECT e.department.name, COUNT(e) FROM Employee e GROUP BY e.department.name")
+    List<Object[]> countEmployeesByDepartment();
+
+
+    @Query("SELECT COUNT(e) FROM Employee e")
+    long countTotalEmployees();
 }
